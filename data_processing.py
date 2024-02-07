97c5a3fcc497a845d60a7ca415a77961674ac31e
Marco Lomele <mark.lomele@gmail.com>
2024-02-07 19:23:46 +0100

    correlation analysis

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,13 @@
+
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 from datetime import datetime
 import numpy as np
+import seaborn as sns
 
-### Parameters ###
-
-#If using absolute sum filter
-top_filter = 30
-
-### Italy ###
+ 
+# ## Italy
 
 #Adding header entry
 file_path_cases = 'data/Italy/italy_labels.csv'
@@ -23,23 +22,28 @@ with open(file_path_cases, 'w') as f:
     f.writelines(content)
     f.close()
 
+
 #Cleaning columns
 cases_ita_df = pd.read_csv(file_path_cases)
 cases_ita_df = cases_ita_df.drop(columns=['index', 'id'])
+cases_ita_df = cases_ita_df.iloc[:, :80]
+
 
 #Fix Bergamo outlier at 2020-05-12 with average of week
 cases_ita_df.at[12, '2020-05-12'] = int(cases_ita_df.iloc[12, 72:79].mean())
+
 
 #Selecting subset of cities by percentile.
 #upper_quant_ita = cases_ita_df.iloc[:, 1:].max(axis=1).quantile(0.75)
 #cases_ita_df = cases_ita_df[cases_ita_df.iloc[:, 1:].ge(upper_quant_ita).any(axis=1)]
 
 #Selecting subset of cities by number of absoluet cases.
-top_ita = cases_ita_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(top_filter).index
-cases_ita_df = cases_ita_df.loc[top_ita]
+top_30_ita = cases_ita_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(30).index
+cases_ita_df = cases_ita_df.loc[top_30_ita]
 selected_cities_ita = cases_ita_df['name'].to_numpy(dtype='str')
 
-### Spain ###
+ 
+# ## Spain
 
 #Importing CSV
 cases_spa_df = pd.read_csv('data/Spain/spain_labels.csv')
@@ -48,42 +52,50 @@ cases_spa_df[cases_spa_df.columns[1:]] = cases_spa_df.iloc[:, 1:].astype('int64'
 #Outlier due to mis-reporting
 cases_spa_df = cases_spa_df.drop(index = 51)
 
+
 #Selecting subset of cities by quantile
 #upper_quant_spa = cases_spa_df.iloc[:, 1:].mean(axis=1).quantile(0.80)
 #cases_spa_df = cases_spa_df[cases_spa_df.iloc[:, 1:].ge(upper_quant_spa).any(axis=1)]
 
 #Selecting subset by absolute number of cases.
-top_spa = cases_spa_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(top_filter).index
-cases_spa_df = cases_spa_df.loc[top_spa]
+top_30_spa = cases_spa_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(30).index
+cases_spa_df = cases_spa_df.loc[top_30_spa]
 selected_cities_spa = cases_spa_df['name'].to_numpy(dtype='str')
 
-### France ###
+ 
+# ## France
 
 #Importing CSV
 cases_fra_df = pd.read_csv('data/France/france_labels.csv')
+
 
 #Selecting subset of cities by quantiles
 #upper_quant_fra = cases_fra_df.iloc[:, 1:].mean(axis=1).quantile(0.8)
 #cases_fra_df = cases_fra_df[cases_fra_df.iloc[:, 1:].ge(upper_quant_fra).any(axis=1)]
 
 #Selecting cities by absolute number of cases
-top_fra = cases_fra_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(top_filter).index
-cases_fra_df = cases_fra_df.loc[top_fra]
+top_30_fra = cases_fra_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(30).index
+cases_fra_df = cases_fra_df.loc[top_30_fra]
 selected_cities_fra = cases_fra_df['name'].to_numpy(dtype='str')
 
-### England ###
+ 
+# ## England
+
 cases_eng_df = pd.read_csv('data/England/england_labels.csv')
+
 
 #Selecting cities by quantiles
 #upper_quant_eng = cases_eng_df.iloc[:, 1:].mean(axis=1).quantile(0.75)
 #cases_eng_df = cases_eng_df[cases_eng_df.iloc[:, 1:].ge(upper_quant_eng).any(axis=1)]
 
 #Selecting subset of cities by absolute value
-top_eng = cases_eng_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=True).head(top_filter).index
-cases_eng_df = cases_eng_df.loc[top_eng]
+top_30_eng = cases_eng_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=True).head(30).index
+cases_eng_df = cases_eng_df.loc[top_30_eng]
 selected_cities_eng = cases_eng_df['name'].to_numpy(dtype='str')
 
-### Aggregating ###
+ 
+# # Aggregating
+
 
 selected_cities_list = [selected_cities_ita, selected_cities_spa, selected_cities_fra, selected_cities_eng]
 cases_dfs_list = [cases_ita_df, cases_spa_df, cases_fra_df, cases_eng_df]
@@ -95,6 +107,7 @@ folder_path_dict = {
     'eng' : 'data/England/graphs',
 }
 movement_dfs_list = []
+
 
 for i in range(len(countries)):
     movement_df = pd.DataFrame(columns=['src', 'trg', 'movement', 'date'])
@@ -131,12 +144,14 @@ for i in range(len(countries)):
     movement_df = movement_df.reset_index(drop=True)
     movement_dfs_list.append(movement_df)
 
+
 movement_ita_df = movement_dfs_list[0]
 movement_spa_df = movement_dfs_list[1]
 movement_fra_df = movement_dfs_list[2]
 movement_eng_df = movement_dfs_list[3]
 
-### Indexing ###
+ 
+# # Indexing
 
 selected_cities_ita = np.sort(selected_cities_ita)
 selected_cities_spa = np.sort(selected_cities_spa)
@@ -150,9 +165,24 @@ mapping_eng = {city: i for i, city in enumerate(selected_cities_eng, start=1)}
 
 movement_ita_df['src'] = movement_ita_df['src'].map(mapping_ita)
 movement_ita_df['trg'] = movement_ita_df['trg'].map(mapping_ita)
+cases_ita_df['name'] = cases_ita_df['name'].map(mapping_ita)
+cases_ita_df = cases_ita_df.sort_values(by=['name'])
+cases_ita_df = cases_ita_df.reset_index(drop=True)
+
 movement_spa_df['src'] = movement_spa_df['src'].map(mapping_spa)
 movement_spa_df['trg'] = movement_spa_df['trg'].map(mapping_spa)
+cases_spa_df['name'] = cases_spa_df['name'].map(mapping_spa)
+cases_spa_df = cases_spa_df.sort_values(by=['name'])
+cases_spa_df = cases_spa_df.reset_index(drop=True)
+
 movement_fra_df['src'] = movement_fra_df['src'].map(mapping_fra)
 movement_fra_df['trg'] = movement_fra_df['trg'].map(mapping_fra)
+cases_fra_df['name'] = cases_fra_df['name'].map(mapping_fra)
+cases_fra_df = cases_fra_df.sort_values(by=['name'])
+cases_fra_df = cases_fra_df.reset_index(drop=True)
+
 movement_eng_df['src'] = movement_eng_df['src'].map(mapping_eng)
 movement_eng_df['trg'] = movement_eng_df['trg'].map(mapping_eng)
+cases_eng_df['name'] = cases_eng_df['name'].map(mapping_eng)
+cases_eng_df = cases_eng_df.sort_values(by=['name'])
+cases_eng_df = cases_eng_df.reset_index(drop=True)
