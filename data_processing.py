0520d4977a80719d576906cb2e4ae971a356b467
Marco Lomele <mark.lomele@gmail.com>
2024-02-12 12:17:31 +0100

    normalisation and frequency

--- data_processing.py
+++ data_processing.py
@@ -1,17 +1,18 @@
 
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
 from datetime import datetime
 import numpy as np
-import seaborn as sns
+import itertools
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
  
-# ## Italy
+## Italy
 
-#Adding header entry
+#Adding header
 file_path_cases = 'data/Italy/italy_labels.csv'
 
 with open(file_path_cases, 'r') as f:
@@ -24,20 +25,15 @@ with open(file_path_cases, 'w') as f:
     f.writelines(content)
     f.close()
 
-
 #Cleaning columns
 cases_ita_df = pd.read_csv(file_path_cases)
 cases_ita_df = cases_ita_df.drop(columns=['index', 'id'])
 cases_ita_df = cases_ita_df.iloc[:, :80]
+cases_ita_df[cases_ita_df.columns[1:]] = cases_ita_df.iloc[:, 1:].astype('float64')
 
 
 #Fix Bergamo outlier at 2020-05-12 with average of week
 cases_ita_df.at[12, '2020-05-12'] = int(cases_ita_df.iloc[12, 72:79].mean())
-
-
-#Selecting subset of cities by percentile.
-#upper_quant_ita = cases_ita_df.iloc[:, 1:].max(axis=1).quantile(0.75)
-#cases_ita_df = cases_ita_df[cases_ita_df.iloc[:, 1:].ge(upper_quant_ita).any(axis=1)]
 
 #Selecting subset of cities by number of absoluet cases.
 top_30_ita = cases_ita_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(30).index
@@ -45,35 +41,22 @@ cases_ita_df = cases_ita_df.loc[top_30_ita]
 selected_cities_ita = cases_ita_df['name'].to_numpy(dtype='str')
 
  
-# ## Spain
-
-#Importing CSV
+## Spain
 cases_spa_df = pd.read_csv('data/Spain/spain_labels.csv')
-cases_spa_df[cases_spa_df.columns[1:]] = cases_spa_df.iloc[:, 1:].astype('int64')
+cases_spa_df[cases_spa_df.columns[1:]] = cases_spa_df.iloc[:, 1:].astype('float64')
 
 #Outlier due to mis-reporting
 cases_spa_df = cases_spa_df.drop(index = 51)
-
-
-#Selecting subset of cities by quantile
-#upper_quant_spa = cases_spa_df.iloc[:, 1:].mean(axis=1).quantile(0.80)
-#cases_spa_df = cases_spa_df[cases_spa_df.iloc[:, 1:].ge(upper_quant_spa).any(axis=1)]
 
 #Selecting subset by absolute number of cases.
 top_30_spa = cases_spa_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(30).index
 cases_spa_df = cases_spa_df.loc[top_30_spa]
 selected_cities_spa = cases_spa_df['name'].to_numpy(dtype='str')
 
- 
-# ## France
 
-#Importing CSV
+## France
 cases_fra_df = pd.read_csv('data/France/france_labels.csv')
-
-
-#Selecting subset of cities by quantiles
-#upper_quant_fra = cases_fra_df.iloc[:, 1:].mean(axis=1).quantile(0.8)
-#cases_fra_df = cases_fra_df[cases_fra_df.iloc[:, 1:].ge(upper_quant_fra).any(axis=1)]
+cases_fra_df[cases_fra_df.columns[1:]] = cases_fra_df.iloc[:, 1:].astype('float64')
 
 #Selecting cities by absolute number of cases
 top_30_fra = cases_fra_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=False).head(30).index
@@ -81,14 +64,9 @@ cases_fra_df = cases_fra_df.loc[top_30_fra]
 selected_cities_fra = cases_fra_df['name'].to_numpy(dtype='str')
 
  
-# ## England
-
+## England
 cases_eng_df = pd.read_csv('data/England/england_labels.csv')
-
-
-#Selecting cities by quantiles
-#upper_quant_eng = cases_eng_df.iloc[:, 1:].mean(axis=1).quantile(0.75)
-#cases_eng_df = cases_eng_df[cases_eng_df.iloc[:, 1:].ge(upper_quant_eng).any(axis=1)]
+cases_eng_df[cases_eng_df.columns[1:]] = cases_eng_df.iloc[:, 1:].astype('float64')
 
 #Selecting subset of cities by absolute value
 top_30_eng = cases_eng_df.iloc[:, 1:].sum(axis=1).sort_values(ascending=True).head(30).index
@@ -96,9 +74,7 @@ cases_eng_df = cases_eng_df.loc[top_30_eng]
 selected_cities_eng = cases_eng_df['name'].to_numpy(dtype='str')
 
  
-# # Aggregating
-
-
+## Aggregating
 selected_cities_list = [selected_cities_ita, selected_cities_spa, selected_cities_fra, selected_cities_eng]
 cases_dfs_list = [cases_ita_df, cases_spa_df, cases_fra_df, cases_eng_df]
 countries = ['ita', 'spa', 'fra', 'eng']
@@ -109,7 +85,6 @@ folder_path_dict = {
     'eng' : 'data/England/graphs',
 }
 movement_dfs_list = []
-
 
 for i in range(len(countries)):
     movement_df = pd.DataFrame(columns=['src', 'trg', 'movement', 'date'])
@@ -131,7 +106,7 @@ for i in range(len(countries)):
             f.close()
 
         df = pd.read_csv(file_path)
-        df = df.astype({'src':'str', 'trg':'str', 'movement':'int64'})
+        df = df.astype({'src':'str', 'trg':'str', 'movement':'float64'})
         df = df[(df['src'].isin(selected_cities_list[i])) & (df['trg'].isin(selected_cities_list[i]))]
 
         year, month, day = int(filename[3:-10]), int(filename[8:-7]), int(filename[11:-4])
@@ -146,15 +121,12 @@ for i in range(len(countries)):
     movement_df = movement_df.reset_index(drop=True)
     movement_dfs_list.append(movement_df)
 
-
 movement_ita_df = movement_dfs_list[0]
 movement_spa_df = movement_dfs_list[1]
 movement_fra_df = movement_dfs_list[2]
 movement_eng_df = movement_dfs_list[3]
 
- 
-# # Indexing
-
+## Indexing
 selected_cities_ita = np.sort(selected_cities_ita)
 selected_cities_spa = np.sort(selected_cities_spa)
 selected_cities_fra = np.sort(selected_cities_fra)
@@ -165,26 +137,57 @@ mapping_spa = {city: i for i, city in enumerate(selected_cities_spa, start=0)}
 mapping_fra = {city: i for i, city in enumerate(selected_cities_fra, start=0)}
 mapping_eng = {city: i for i, city in enumerate(selected_cities_eng, start=0)}
 
-movement_ita_df['src'] = movement_ita_df['src'].map(mapping_ita)
-movement_ita_df['trg'] = movement_ita_df['trg'].map(mapping_ita)
-cases_ita_df['name'] = cases_ita_df['name'].map(mapping_ita)
-cases_ita_df = cases_ita_df.sort_values(by=['name'])
-cases_ita_df = cases_ita_df.reset_index(drop=True)
+def indexing_data(movement_df, cases_df, mapping):
+    movement_df['src'] = movement_df['src'].map(mapping)
+    movement_df['trg'] = movement_df['trg'].map(mapping)
+    cases_df['name'] = cases_df['name'].map(mapping)
+    cases_df = cases_df.sort_values(by=['name']).reset_index(drop=True)
+    return movement_df, cases_df
 
-movement_spa_df['src'] = movement_spa_df['src'].map(mapping_spa)
-movement_spa_df['trg'] = movement_spa_df['trg'].map(mapping_spa)
-cases_spa_df['name'] = cases_spa_df['name'].map(mapping_spa)
-cases_spa_df = cases_spa_df.sort_values(by=['name'])
-cases_spa_df = cases_spa_df.reset_index(drop=True)
+movement_ita_df, cases_ita_df = indexing_data(movement_ita_df, cases_ita_df, mapping_ita)
+movement_spa_df, cases_spa_df = indexing_data(movement_spa_df, cases_spa_df, mapping_spa)
+movement_fra_df, cases_fra_df = indexing_data(movement_fra_df, cases_fra_df, mapping_fra)
+movement_eng_df, cases_eng_df = indexing_data(movement_eng_df, cases_eng_df, mapping_eng)
 
-movement_fra_df['src'] = movement_fra_df['src'].map(mapping_fra)
-movement_fra_df['trg'] = movement_fra_df['trg'].map(mapping_fra)
-cases_fra_df['name'] = cases_fra_df['name'].map(mapping_fra)
-cases_fra_df = cases_fra_df.sort_values(by=['name'])
-cases_fra_df = cases_fra_df.reset_index(drop=True)
 
-movement_eng_df['src'] = movement_eng_df['src'].map(mapping_eng)
-movement_eng_df['trg'] = movement_eng_df['trg'].map(mapping_eng)
-cases_eng_df['name'] = cases_eng_df['name'].map(mapping_eng)
-cases_eng_df = cases_eng_df.sort_values(by=['name'])
-cases_eng_df = cases_eng_df.reset_index(drop=True)
+## Connecting
+# Function for generating fully connected dataframes for fully connected graphs. 
+def generate_df_connected_movement(movement_df):
+    dates = movement_df['date'].unique()
+    cities = list(range(0, 30))
+    all_combinations = pd.DataFrame(list(itertools.product(dates, cities, cities)), columns=['date', 'src', 'trg'])
+    connected_movement_df = all_combinations.merge(movement_df, on=['date', 'src', 'trg'], how='left')
+    connected_movement_df['movement'] = connected_movement_df['movement'].fillna(0)
+    connected_movement_df['movement'] = connected_movement_df['movement'].astype(int)
+    return connected_movement_df
+
+movement_ita_df = generate_df_connected_movement(movement_ita_df)
+movement_spa_df = generate_df_connected_movement(movement_spa_df)
+movement_fra_df = generate_df_connected_movement(movement_fra_df)
+movement_eng_df = generate_df_connected_movement(movement_eng_df)
+
+def calc_positive_edge_freq(movement_df):
+    movement_df['is_positive'] = movement_df['movement'] > 0
+    movement_df['cumsum_positive'] = movement_df.groupby(['src', 'trg'])['is_positive'].cumsum()
+    movement_df['positive_freq'] = movement_df.groupby(['src', 'trg']).cumcount()
+    movement_df['positive_freq'] = movement_df['cumsum_positive'] / (movement_df['positive_freq'] + 1)
+    movement_df.drop(columns=['is_positive', 'cumsum_positive'], inplace=True)
+
+    return movement_df
+
+movement_ita_df = calc_positive_edge_freq(movement_ita_df)
+movement_spa_df = calc_positive_edge_freq(movement_spa_df)
+movement_fra_df = calc_positive_edge_freq(movement_fra_df)
+movement_eng_df = calc_positive_edge_freq(movement_eng_df) 
+
+
+## Normalising
+cases_ita_df.iloc[:, 1:] = np.log(cases_ita_df.iloc[:, 1:]+1)
+cases_spa_df.iloc[:, 1:] = np.log(cases_spa_df.iloc[:, 1:]+1)
+cases_fra_df.iloc[:, 1:] = np.log(cases_fra_df.iloc[:, 1:]+1)
+cases_eng_df.iloc[:, 1:] = np.log(cases_eng_df.iloc[:, 1:]+1)
+
+movement_ita_df['movement'] = np.log(movement_ita_df['movement'].values + 1)
+movement_spa_df['movement'] = np.log(movement_spa_df['movement'].values + 1)
+movement_fra_df['movement'] = np.log(movement_fra_df['movement'].values + 1)
+movement_eng_df['movement'] = np.log(movement_eng_df['movement'].values + 1)
--- graph_generation.py
+++ graph_generation.py
@@ -1,54 +1,35 @@
 from data_processing import movement_ita_df, movement_spa_df, movement_fra_df, movement_eng_df
 from data_processing import cases_ita_df, cases_spa_df, cases_fra_df, cases_eng_df
 import pandas as pd
-import itertools
-from datetime import datetime
 from torch_geometric.data import Data
 import torch
 import numpy as np
 
-
-def generate_complete_movement_df(movement_df):
-    dates = movement_df['date'].unique()
-    cities = list(range(1, 31))
-    all_combinations = pd.DataFrame(list(itertools.product(dates, cities, cities)), columns=['date', 'src', 'trg'])
-    complete_movement_df = all_combinations.merge(
-        movement_df, on=['date', 'src', 'trg'], how='left'
-    )
-    complete_movement_df['movement'] = complete_movement_df['movement'].fillna(0)
-    complete_movement_df['movement'] = complete_movement_df['movement'].astype(int)
-    return complete_movement_df
-
-
-def generate_snapshots_dfs_dict(movement_df, cases_df):
+def generate_dict_graph_snapshots(movement_df, cases_df):
     dates = movement_df['date'].unique()
     snapshots_dict = {}
 
-    for i in range(len(dates)):
-        date = dates[i]
-        df = movement_df[movement_df['date']==date]
+    for i in range(6,len(dates)):
+        df = movement_df[movement_df['date']==dates[i-6]]
 
         edge_index_arr = np.vstack((df['src'].values, df['trg'].values))
         edge_index = torch.tensor(edge_index_arr, dtype=torch.long)
-        edge_attr = torch.tensor(df['movement'].values[:, None], dtype=torch.float)
+        edge_weight = torch.tensor(df['movement'].values[:, None], dtype=torch.float)
+        edge_weight_index = edge_index.detach().clone()
+        edge_attr = torch.tensor(df['positive_freq'].values[:, None], dtype=torch.float)
         num_nodes = df['src'].max()
-        node_features = torch.tensor(cases_df[str(date)[:10]].values, dtype=torch.float)
         
-        graph = Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr, num_nodes=num_nodes)
+        node_features = torch.tensor(cases_df.iloc[:, i-6:i+1].values, dtype=torch.float)
+        
+        graph = Data(x=node_features, edge_index=edge_index, edge_weight=edge_weight, 
+                     edge_weight_index=edge_weight_index, edge_attr=edge_attr, num_nodes=num_nodes)
 
-        snapshots_dict[date] = graph
+        snapshots_dict[dates[i]] = graph
         
     return snapshots_dict
 
-
-#Creating fully connecyed data frames for fully connected graphs.
-complete_movement_ita_df = generate_complete_movement_df(movement_ita_df)
-complete_movement_spa_df = generate_complete_movement_df(movement_spa_df)
-complete_movement_fra_df = generate_complete_movement_df(movement_fra_df)
-complete_movement_eng_df = generate_complete_movement_df(movement_eng_df)
-
 #Dictionaries with daily graphs for each country.
-daily_graphs_ita_dict = generate_snapshots_dfs_dict(complete_movement_ita_df, cases_ita_df)
-daily_graphs_spa_dict = generate_snapshots_dfs_dict(complete_movement_spa_df, cases_spa_df)
-daily_graphs_fra_dict = generate_snapshots_dfs_dict(complete_movement_fra_df, cases_fra_df)
-daily_graphs_eng_dict = generate_snapshots_dfs_dict(complete_movement_eng_df, cases_eng_df)
+daily_graphs_ita_dict = generate_dict_graph_snapshots(movement_ita_df, cases_ita_df)
+daily_graphs_spa_dict = generate_dict_graph_snapshots(movement_spa_df, cases_spa_df)
+daily_graphs_fra_dict = generate_dict_graph_snapshots(movement_fra_df, cases_fra_df)
+daily_graphs_eng_dict = generate_dict_graph_snapshots(movement_eng_df, cases_eng_df)
