8ecdc4d510a9f3993d208ed56492f45a6781f750
Marco Lomele <mark.lomele@gmail.com>
2024-02-17 13:19:17 +0100

    added a3tgcn

--- pytorch_geometric_temporal_models.py
+++ pytorch_geometric_temporal_models.py
@@ -3,7 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
+
 ### MPNN LSTM ###
+
 class MPNNLSTM(nn.Module):
     r"""An implementation of the Message Passing Neural Network with Long Short Term Memory.
     For details see this paper: `"Transfer Graph Neural Networks for Pandemic Forecasting." <https://arxiv.org/abs/2009.08388>`_
@@ -104,6 +106,7 @@ class MPNNLSTM(nn.Module):
         H = torch.cat([H_1[0, :, :], H_2[0, :, :], S], dim=1)
         return H
     
+
 ### EVOLVEGCN-H ###
     
 from torch.nn import GRU
@@ -111,7 +114,6 @@ from torch_geometric.nn import TopKPooling
 from torch_geometric.nn.inits import glorot
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.conv.gcn_conv import gcn_norm
-
 
 class GCNConv_Fixed_W(MessagePassing):
     r"""The graph convolutional operator adapted from the `"Semi-supervised
@@ -292,3 +294,206 @@ class EvolveGCNH(torch.nn.Module):
         X_tilde, W = self.recurrent_layer(X_tilde, W)
         X = self.conv_layer(W.squeeze(dim=0), X, edge_index, edge_weight)
         return X
+    
+### A3TGCN ###
+    
+class TGCN(torch.nn.Module):
+    r"""An implementation of the Temporal Graph Convolutional Gated Recurrent Cell.
+    For details see this paper: `"T-GCN: A Temporal Graph ConvolutionalNetwork for
+    Traffic Prediction." <https://arxiv.org/abs/1811.05320>`_
+
+    Args:
+        in_channels (int): Number of input features.
+        out_channels (int): Number of output features.
+        improved (bool): Stronger self loops. Default is False.
+        cached (bool): Caching the message weights. Default is False.
+        add_self_loops (bool): Adding self-loops for smoothing. Default is True.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        improved: bool = False,
+        cached: bool = False,
+        add_self_loops: bool = True,
+    ):
+        super(TGCN, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.improved = improved
+        self.cached = cached
+        self.add_self_loops = add_self_loops
+
+        self._create_parameters_and_layers()
+
+    def _create_update_gate_parameters_and_layers(self):
+
+        self.conv_z = GCNConv(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            improved=self.improved,
+            cached=self.cached,
+            add_self_loops=self.add_self_loops,
+        )
+
+        self.linear_z = torch.nn.Linear(2 * self.out_channels, self.out_channels)
+
+    def _create_reset_gate_parameters_and_layers(self):
+
+        self.conv_r = GCNConv(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            improved=self.improved,
+            cached=self.cached,
+            add_self_loops=self.add_self_loops,
+        )
+
+        self.linear_r = torch.nn.Linear(2 * self.out_channels, self.out_channels)
+
+    def _create_candidate_state_parameters_and_layers(self):
+
+        self.conv_h = GCNConv(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            improved=self.improved,
+            cached=self.cached,
+            add_self_loops=self.add_self_loops,
+        )
+
+        self.linear_h = torch.nn.Linear(2 * self.out_channels, self.out_channels)
+
+    def _create_parameters_and_layers(self):
+        self._create_update_gate_parameters_and_layers()
+        self._create_reset_gate_parameters_and_layers()
+        self._create_candidate_state_parameters_and_layers()
+
+    def _set_hidden_state(self, X, H):
+        if H is None:
+            H = torch.zeros(X.shape[0], self.out_channels).to(X.device)
+        return H
+
+    def _calculate_update_gate(self, X, edge_index, edge_weight, H):
+        Z = torch.cat([self.conv_z(X, edge_index, edge_weight), H], axis=1)
+        Z = self.linear_z(Z)
+        Z = torch.sigmoid(Z)
+        return Z
+
+    def _calculate_reset_gate(self, X, edge_index, edge_weight, H):
+        R = torch.cat([self.conv_r(X, edge_index, edge_weight), H], axis=1)
+        R = self.linear_r(R)
+        R = torch.sigmoid(R)
+        return R
+
+    def _calculate_candidate_state(self, X, edge_index, edge_weight, H, R):
+        H_tilde = torch.cat([self.conv_h(X, edge_index, edge_weight), H * R], axis=1)
+        H_tilde = self.linear_h(H_tilde)
+        H_tilde = torch.tanh(H_tilde)
+        return H_tilde
+
+    def _calculate_hidden_state(self, Z, H, H_tilde):
+        H = Z * H + (1 - Z) * H_tilde
+        return H
+
+    def forward(
+        self,
+        X: torch.FloatTensor,
+        edge_index: torch.LongTensor,
+        edge_weight: torch.FloatTensor = None,
+        H: torch.FloatTensor = None,
+    ) -> torch.FloatTensor:
+        """
+        Making a forward pass. If edge weights are not present the forward pass
+        defaults to an unweighted graph. If the hidden state matrix is not present
+        when the forward pass is called it is initialized with zeros.
+
+        Arg types:
+            * **X** *(PyTorch Float Tensor)* - Node features.
+            * **edge_index** *(PyTorch Long Tensor)* - Graph edge indices.
+            * **edge_weight** *(PyTorch Long Tensor, optional)* - Edge weight vector.
+            * **H** *(PyTorch Float Tensor, optional)* - Hidden state matrix for all nodes.
+
+        Return types:
+            * **H** *(PyTorch Float Tensor)* - Hidden state matrix for all nodes.
+        """
+        H = self._set_hidden_state(X, H)
+        Z = self._calculate_update_gate(X, edge_index, edge_weight, H)
+        R = self._calculate_reset_gate(X, edge_index, edge_weight, H)
+        H_tilde = self._calculate_candidate_state(X, edge_index, edge_weight, H, R)
+        H = self._calculate_hidden_state(Z, H, H_tilde)
+        return H
+
+class A3TGCN(torch.nn.Module):
+    r"""An implementation of the Attention Temporal Graph Convolutional Cell.
+    For details see this paper: `"A3T-GCN: Attention Temporal Graph Convolutional
+    Network for Traffic Forecasting." <https://arxiv.org/abs/2006.11583>`_
+
+    Args:
+        in_channels (int): Number of input features.
+        out_channels (int): Number of output features.
+        periods (int): Number of time periods.
+        improved (bool): Stronger self loops (default :obj:`False`).
+        cached (bool): Caching the message weights (default :obj:`False`).
+        add_self_loops (bool): Adding self-loops for smoothing (default :obj:`True`).
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        periods: int,
+        improved: bool = False,
+        cached: bool = False,
+        add_self_loops: bool = True
+    ):
+        super(A3TGCN, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.periods = periods
+        self.improved = improved
+        self.cached = cached
+        self.add_self_loops = add_self_loops
+        self._setup_layers()
+
+    def _setup_layers(self):
+        self._base_tgcn = TGCN(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            improved=self.improved,
+            cached=self.cached,
+            add_self_loops=self.add_self_loops,
+        )
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self._attention = torch.nn.Parameter(torch.empty(self.periods, device=device))
+        torch.nn.init.uniform_(self._attention)
+
+    def forward(
+        self,
+        X: torch.FloatTensor,
+        edge_index: torch.LongTensor,
+        edge_weight: torch.FloatTensor = None,
+        H: torch.FloatTensor = None,
+    ) -> torch.FloatTensor:
+        """
+        Making a forward pass. If edge weights are not present the forward pass
+        defaults to an unweighted graph. If the hidden state matrix is not present
+        when the forward pass is called it is initialized with zeros.
+
+        Arg types:
+            * **X** (PyTorch Float Tensor): Node features for T time periods.
+            * **edge_index** (PyTorch Long Tensor): Graph edge indices.
+            * **edge_weight** (PyTorch Long Tensor, optional)*: Edge weight vector.
+            * **H** (PyTorch Float Tensor, optional): Hidden state matrix for all nodes.
+
+        Return types:
+            * **H** (PyTorch Float Tensor): Hidden state matrix for all nodes.
+        """
+        H_accum = 0
+        probs = torch.nn.functional.softmax(self._attention, dim=0)
+        for period in range(self.periods):
+            H_accum = H_accum + probs[period] * self._base_tgcn(
+                X[:, :, period], edge_index, edge_weight, H
+            )
+        return H_accum
