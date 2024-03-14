bae8174e92ce61527adc64519a3fbd842b666111
Marco Lomele <mark.lomele@gmail.com>
2024-03-14 15:29:28 +0100

    negative sampling edit

--- graph_generation.py
+++ graph_generation.py
@@ -15,7 +15,7 @@ cases_spa_df = cases_dfs_dict['cases_spa_df']
 cases_fra_df = cases_dfs_dict['cases_fra_df']
 cases_eng_df = cases_dfs_dict['cases_eng_df']
 
-def generate_dict_graph_snapshots(movement_df, cases_df):
+def generate_dict_graph_snapshots_conn(movement_df, cases_df):
     dates = movement_df['date'].unique()
     snapshots_dict = {}
 
@@ -38,11 +38,40 @@ def generate_dict_graph_snapshots(movement_df, cases_df):
         
     return snapshots_dict
 
-#Dictionaries with daily graphs for each country.
-#daily_graphs_ita_dict = generate_dict_graph_snapshots(movement_ita_df, cases_ita_df)
-#daily_graphs_spa_dict = generate_dict_graph_snapshots(movement_spa_df, cases_spa_df)
-#daily_graphs_fra_dict = generate_dict_graph_snapshots(movement_fra_df, cases_fra_df)
-#daily_graphs_eng_dict = generate_dict_graph_snapshots(movement_eng_df, cases_eng_df)
+#Dictionaries with connected daily graphs for each country. Connected => all nodes are connected in each snapshot, hence high sparisty.
+snapshots_graphs_conn_ita_dict = generate_dict_graph_snapshots_conn(movement_ita_df, cases_ita_df)
+snapshots_graphs_conn_spa_dict = generate_dict_graph_snapshots_conn(movement_spa_df, cases_spa_df)
+snapshots_graphs_conn_fra_dict = generate_dict_graph_snapshots_conn(movement_fra_df, cases_fra_df)
+snapshots_graphs_conn_eng_dict = generate_dict_graph_snapshots_conn(movement_eng_df, cases_eng_df)
+
+def generate_dict_graph_snapshots_unco(movement_df, cases_df):
+    movement_df = movement_df[movement_df['movement']>0].copy()
+    dates = movement_df['date'].unique()
+    snapshots_dict = {}
+
+    for i in range(7,len(dates)):
+        df = movement_df[movement_df['date']==dates[i-7]]
+        
+        edge_index_arr = np.vstack((df['src'].values, df['trg'].values))
+        edge_index = torch.tensor(edge_index_arr, dtype=torch.long)
+        edge_weights = torch.tensor(df['movement'].values[:, None], dtype=torch.float)
+        edge_weights_index = edge_index.detach().clone()
+        num_nodes = df['src'].max() + 1
+        
+        node_features = torch.tensor(cases_df.iloc[:, i-6:i+1].values, dtype=torch.float)
+        
+        graph = Data(x=node_features, edge_index=edge_index, edge_weights=edge_weights, 
+                     edge_weights_index=edge_weights_index, num_nodes=num_nodes)
+
+        snapshots_dict[dates[i]] = graph
+        
+    return snapshots_dict
+
+#Dictionaries with unconnected daily graphs for each country. Unconnected => graphs don't have dummy edges of movemement 0, hence no sparisty.
+snapshots_graphs_unco_ita_dict = generate_dict_graph_snapshots_unco(movement_ita_df, cases_ita_df)
+snapshots_graphs_unco_spa_dict = generate_dict_graph_snapshots_unco(movement_spa_df, cases_spa_df)
+snapshots_graphs_unco_fra_dict = generate_dict_graph_snapshots_unco(movement_fra_df, cases_fra_df)
+snapshots_graphs_unco_eng_dict = generate_dict_graph_snapshots_unco(movement_eng_df, cases_eng_df)
 
 #Generaiton of temporal data (not fully conencted)
 def generate_td_movement_df(movement_df):
